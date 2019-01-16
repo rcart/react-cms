47da719ba1c45b75d0ea682d59f75ca53e4b42e2
Ronny Cardona <rcart19@gmail.com>
2019-01-15 18:38:46 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.bundle.js'
+  },
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(css)$/, use: [ 'style-loader', 'css-loader' ] }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+};
