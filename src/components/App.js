a22947bd4452b9f47d369a1427ab1d8cc961633c
Ronny Cardona <rcart19@gmail.com>
2019-01-16 23:26:35 -0600

    Started to work on Hero component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from './Hero';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <Hero />
     );
   }
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="hero">
+          <h1>Ronny Cardona</h1>
+          <h3>@rcart19</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hero;
