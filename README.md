4794f6d4bab699861690e73ab1cdc1aeca14d30f
Ronny Cardona <rcart19@gmail.com>
2019-01-17 18:53:31 -0600

    Completed Hero Component and added some CSS styling

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flat-file CMS
+Basic Content Management System using ReactJS
+
+# WIP
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,20 @@ class Hero extends React.Component {
     return (
       <div className="container">
         <div className="hero">
-          <h1>Ronny Cardona</h1>
-          <h3>@rcart19</h3>
+          <img className="hero-pict" src="./assets/homer-hero.jpg" alt="Smart Homer Simpson" />
+          <h1 className="hero-title">Ronny Cardona</h1>
+          <h4 className="hero-twitter">Frontend Web Developer</h4>
+          <div className="hero-social">
+            <a href="https://github.com/rcart" target="_blank">
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="https://twitter.com/rcart19" target="_blank">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/rjcardona" target="_blank">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
         </div>
       </div>
     );
